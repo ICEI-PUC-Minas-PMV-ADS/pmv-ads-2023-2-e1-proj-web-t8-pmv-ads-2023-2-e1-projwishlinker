19358f42f17357ffa256b3803f60237b84cc111d
Thiago Pires <thiagopac@gmail.com>
2023-11-13 09:19:51 -0300

    Recuperando listas de desejos e quarentena de compras no dashboard para apresentar dados consolidados

--- codigo-fonte/scripts/dashboard.js
+++ codigo-fonte/scripts/dashboard.js
@@ -1,5 +1,14 @@
 // @ts-nocheck
 
 $(document).ready(function () {
+  const listaDeDesejos =
+    JSON.parse(localStorage.getItem("listaDeDesejos")) || [];
+  const quarentenaDeCompras =
+    JSON.parse(localStorage.getItem("quarentenaDeCompras")) || [];
 
+  const quantidadeListaDesejos = listaDeDesejos.length;
+  const quantidadeQuarentena = quarentenaDeCompras.length;
+
+  $("#quantidadeListaDesejos").text(quantidadeListaDesejos);
+  $("#quantidadeQuarentena").text(quantidadeQuarentena);
 });
